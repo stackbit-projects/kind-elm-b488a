f27c62aa21b503d97a4eb28a7e72ef1db10e631a
Stackbit Code Editor <projects@stackbit.com>
2021-07-11 21:23:01 +0000

    SectionBackground.js: changed

--- src/components/SectionBackground.js
+++ src/components/SectionBackground.js
@@ -5,6 +5,4 @@ import { withPrefix } from '../utils';
 
 export default class SectionBackground extends React.Component {
     render() {
-        const section = _.get(this.props, 'section');
-        const backgroundImage = _.get(section, 'background_image');
-        const backgroundImageOpacity = _.get(section, 'background_image_opacity', 10
+        
